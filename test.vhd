a0979fd162ecf57cc2d654432840152e06998c44
Christoph Seitz <christoph.seitz@posteo.de>
2019-09-11 19:05:00 +0200

    Update test.

--- test.vhd
+++ test.vhd
@@ -6,3 +6,17 @@ generic (
 port ( signal asd : in std_logic(asd) := '0');
 constant asd : integer := 0;type asdf is range 0 to 5;
 end entity;
+
+architecture asd of gg is
+  component asd
+    generic( asd : tst := 123);
+    port (signal asd:bbb; asd : out fff);
+    end component;
+impure function test(asd : interger := 1) return integer is
+  variable temp : integer := 0;
+begin
+  return temp;
+  end function;
+  begin
+
+  end architecture beh;
