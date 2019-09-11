e154815cc190fcb704d571f9e86b3f0ae6b27a07
Christoph Seitz <christoph.seitz@posteo.de>
2019-09-11 19:11:15 +0200

    Todo

--- lib/Format/FormatLineFormater.cpp
+++ lib/Format/FormatLineFormater.cpp
@@ -8,19 +8,22 @@ namespace detail {
 class LineFormater {
 
 public:
-  LineFormater(FormatTokenIndenter &Indenter) : Indenter(Indenter) {}
+  LineFormater(FormatLineFormater &Formater, FormatTokenIndenter &Indenter)
+      : Formater(Formater), Indenter(Indenter) {}
   virtual ~LineFormater() = default;
 
   virtual Penalty formatLine(const FormatLine &Line) = 0;
 
 protected:
   FormatTokenIndenter &Indenter;
+  FormatLineFormater &Formater;
 };
 
 class NoLineBreakLineFormater : public LineFormater {
 public:
-  NoLineBreakLineFormater(FormatTokenIndenter &Indenter)
-      : LineFormater(Indenter) {}
+  NoLineBreakLineFormater(FormatLineFormater &Formater,
+                          FormatTokenIndenter &Indenter)
+      : LineFormater(Formater, Indenter) {}
 
   virtual Penalty formatLine(const FormatLine &Line) {
     LineState State{Line};
@@ -29,6 +32,9 @@ public:
                      (State.CurrentToken->CanBreakBefore &&
                       State.CurrentToken->NewLinesBefore > 0);
 
+      if (State.CurrentToken->Previous) {
+        Formater.format(State.CurrentToken->Previous->Children);
+      }
       Indenter.addTokenToState(State, Newline);
     }
     return Penalty(0);
@@ -40,7 +46,7 @@ Penalty FormatLineFormater::format(const std::vector<FormatLine *> &Lines) {
   FormatTokenIndenter Indenter{Style, Manager};
 
   for (auto Line : Lines) {
-    detail::NoLineBreakLineFormater Formater{Indenter};
+    detail::NoLineBreakLineFormater Formater{*this, Indenter};
     Formater.formatLine(*Line);
   }
   return Penalty(0);
